1769db3a32e0764348a0196a15c34ce5c1f7bf0f
KSStripes <kristin.schumann@gmail.com>
2023-12-10 11:48:55 +0100

    Initial Commit

---
+++ KSPandarex/OrderBookEntry.cpp
@@ -0,0 +1,8 @@
+//
+//  OrderBookEntry.cpp
+//  KSPandarex
+//
+//  Created by Kristin Schumann on 10/12/2023.
+//
+
+#include "OrderBookEntry.hpp"
---
+++ KSPandarex/OrderBookEntry.hpp
@@ -0,0 +1,13 @@
+//
+//  OrderBookEntry.hpp
+//  KSPandarex
+//
+//  Created by Kristin Schumann on 10/12/2023.
+//
+
+#ifndef OrderBookEntry_hpp
+#define OrderBookEntry_hpp
+
+#include <stdio.h>
+
+#endif /* OrderBookEntry_hpp */
