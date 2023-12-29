7f09f4722b05e90ef15952ce5f522a5164d358ec
KSStripes <kristin.schumann@gmail.com>
2023-12-29 11:57:46 +0100

    Updated PandaMain::printMarketStats() to display table

--- KSPandarex/PandaMain.cpp
+++ KSPandarex/PandaMain.cpp
@@ -13,6 +13,7 @@
 #include "OrderBook.hpp"
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 
 PandaMain::PandaMain(){
@@ -64,45 +65,151 @@ void PandaMain::printHelp(){
 
 /*function for option 2 using OrderBook*/
 void PandaMain::printMarketStats(){
-    Candlestick candlestick(orderBook); // Create a Candlestick instance
+    std::string nextT = orderBook.getNexttime(currentTime);
+    std::string prevT = orderBook.getPreviousTime(currentTime);
+    
+    
+    // Define column width for headers and data
+    int timeWidth = 20;
+    int headerWidth = 15;
+    int dataWidth = 10;
+    
+    // Set the floating-point output to two decimal places
+    std::cout << std::fixed << std::setprecision(2);
 
-        /** Print the candlestick chart - served as dummy test data*/
+    // Print Times
+    std::cout << std::left << std::setw(timeWidth) << "Current Time:" << currentTime << std::endl;
+    std::cout << std::left << std::setw(timeWidth) << "Next Timestamp:" << nextT << std::endl;
+    std::cout << std::left << std::setw(timeWidth) << "Prev Timestamp:" << prevT << std::endl;
+    std::cout << "=================================================================================" << std::endl;
+
+    // Print data headers - product names
+    std::cout << std::left << std::setw(headerWidth) << "Product" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::cout << std::left << std::setw(dataWidth) << p << " | ";
+    }
+    std::cout << std::endl;
+    
+    std::cout << "=================================================================================" << std::endl;
+
+    // Print data for "Total Asks"
+    std::cout << std::left << std::setw(headerWidth) << "Total Asks" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask, p, currentTime);
+        std::cout << std::right << std::setw(dataWidth) << entries.size() << " | ";
+    }
+    std::cout << std::endl;
+
+    // Print data for "Maximum Asks"
+    std::cout << std::left << std::setw(headerWidth) << "Max Asks" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask, p, currentTime);
+        double maxAsk = OrderBook::getHighPrice(entries);
+        std::cout << std::right << std::setw(dataWidth) << maxAsk << " | ";
+    }
+    std::cout << std::endl;
+
+    // Print data for "Minimum Asks"
+    std::cout << std::left << std::setw(headerWidth) << "Min Asks" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask, p, currentTime);
+        double minAsk = OrderBook::getMinPrice(entries);
+        std::cout << std::right << std::setw(dataWidth) << minAsk << " | ";
+    }
+    std::cout << std::endl;
+    
+    // Print data for "Mean Open Ask"
+    std::cout << std::left << std::setw(headerWidth) << "Mean Open Ask" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask, p, currentTime);
+        double meanOpenAsk = orderBook.getMeanOpen(OrderBookType::ask, p, prevT);
+        std::cout << std::right << std::setw(dataWidth) << meanOpenAsk << " | ";
+    }
+    std::cout << std::endl;
+    
+    // Print data for "Mean Close Ask"
+    std::cout << std::left << std::setw(headerWidth) << "Mean Close Ask" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask, p, currentTime);
+        double meanCloseAsk = OrderBook::getMeanPrice(entries);
+        std::cout << std::right << std::setw(dataWidth) << meanCloseAsk << " | ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "=================================================================================" << std::endl;
+
+    // Print data for "Total Bids"
+    std::cout << std::left << std::setw(headerWidth) << "Total Bids" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::bid, p, currentTime);
+        std::cout << std::right << std::setw(dataWidth) << entries.size() << " | ";
+    }
+    std::cout << std::endl;
+
+    // Print data for "Maximum Bids"
+    std::cout << std::left << std::setw(headerWidth) << "Max Bids" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::bid, p, currentTime);
+        double maxAsk = OrderBook::getHighPrice(entries);
+        std::cout << std::right << std::setw(dataWidth) << maxAsk << " | ";
+    }
+    std::cout << std::endl;
+
+    // Print data for "Minimum Bids"
+    std::cout << std::left << std::setw(headerWidth) << "Min Bids" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::bid, p, currentTime);
+        double minAsk = OrderBook::getMinPrice(entries);
+        std::cout << std::right << std::setw(dataWidth) << minAsk << " | ";
+    }
+    std::cout << std::endl;
+    
+    // Print data for "Mean Open Bids"
+    std::cout << std::left << std::setw(headerWidth) << "Mean Open Bid" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::bid, p, currentTime);
+        double meanOpenAsk = orderBook.getMeanOpen(OrderBookType::ask, p, prevT);
+        std::cout << std::right << std::setw(dataWidth) << meanOpenAsk << " | ";
+    }
+    std::cout << std::endl;
+    
+    // Print data for "Mean Close Bids"
+    std::cout << std::left << std::setw(headerWidth) << "Mean Close Bid" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::bid, p, currentTime);
+        double meanCloseAsk = OrderBook::getMeanPrice(entries);
+        std::cout << std::right << std::setw(dataWidth) << meanCloseAsk << " | ";
+    }
+    std::cout << std::endl;
+
+        std::cout << "=================================================================================" << std::endl;
+    
+    // Print data for "Spread"
+    std::cout << std::left << std::setw(headerWidth) << "Spread" << " | ";
+    for (std::string const& p : orderBook.getKnownProducts()) {
+        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::bid, p, currentTime);
+        double meanCloseAsk = orderBook.getSpread(p, currentTime);
+        std::cout << std::right << std::setw(dataWidth) << meanCloseAsk << " | ";
+    }
+    std::cout << std::endl;
+    
+    std::cout << "=================================================================================" << std::endl;
+
+std::cout << std::endl;
+
+}
+
+/**KSStripes implemented the function in userinput to go to options for the candlestick menu**/
+void PandaMain::printCandlesticks() {
+    Candlestick candlestick(orderBook); // Create a Candlestick instance
+    /** Print the candlestick chart - served as dummy test data*/
 //    candlestick.printCandlestickChart(open, high, low, close);
 //    candlestick.printCandlestickChart(5, 8, 3, 6, currentTime);
 //    candlestick.printCandlestickChart(6, 8, 3, 5, currentTime);
 //    candlestick.printCandlestickChart(0.0221, 0.0222, 0.0218, 0.0220, currentTime);
 //    candlestick.printCandlestickChart(119.501, 121.099, 117.329, 119.442, currentTime);
 
-    std::string nextT = orderBook.getNexttime(currentTime);
-    std::string prevT = orderBook.getPreviousTime(currentTime);
     
-    std::cout << "Current Time: " << currentTime << std::endl;
-    std::cout << "Next Timestamp: " << nextT << std::endl;
-    std::cout << "Prev Timestamp: " << prevT << std::endl;
-    std::cout << "========================" << std::endl;
-
-    for (std::string const& p : orderBook.getKnownProducts()){
-        std::cout << "Product: " << p << std::endl;
-        
-        std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask,
-                                                                  p,
-                                                                  currentTime);
-        std::cout << "Asks seen: " << entries.size() << std::endl;
-        std::cout << "Max ask : " << OrderBook::getHighPrice(entries) << std::endl;
-        std::cout << "Min ask : " << OrderBook::getMinPrice(entries) << std::endl;
-        /**KSStripes added function outputs for open, close and  spread*/
-        std::cout << "Mean Open ask : " << orderBook.getMeanOpen(OrderBookType::ask, p, prevT) << std::endl;
-        std::cout << "Mean Close ask : " << OrderBook::getMeanPrice(entries) << std::endl;
-        std::cout << "========================" << std::endl;
-        
-        
-        
-        /**end addition KSStripes**/
-    }
-}
-
-/**KSStripes implemented the function in userinput to go to options for the candlestick menu**/
-void PandaMain::printCandlesticks() {
     UserInput userInput(currentTime, wallet, orderBook);
     userInput.candlestickRequest();
 }
